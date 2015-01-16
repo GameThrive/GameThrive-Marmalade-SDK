4a76fad4012589080bfa20fc6f0d18aea14e9799
Josh Kasten <jkasten@gmail.com>
2015-01-15 19:42:57 -0800

    iOS 64bit + Android conflict fix
    * Added arm64 iOS 64bit binaries.
    * Fixed compatibility issue where GameThrive would conflict with other extensions that used Google Play services or android support library v4.

--- s3eGameThrive/build_s3egamethrive_iphone_iphone_scons_x86/temp_defines_release.h
+++ s3eGameThrive/build_s3egamethrive_iphone_iphone_scons_x86/temp_defines_release.h
@@ -16,6 +16,9 @@
 #ifndef IW_MKF_S3EGAMETHRIVE_BUILD
 #   define IW_MKF_S3EGAMETHRIVE_BUILD 1
 #endif
+#ifndef IW_MKF_S3E_DEFAULT
+#   define IW_MKF_S3E_DEFAULT 1
+#endif
 #ifndef IW_PLATFORM_IPHONE
 #   define IW_PLATFORM_IPHONE 1
 #endif
@@ -36,4 +39,14 @@
 #endif
 #ifndef __IPHONE_OS_VERSION_MIN_REQUIRED
 #   define __IPHONE_OS_VERSION_MIN_REQUIRED 43000
+#endif
+
+/* including "extra_body" specified by the mkb plugin */
+
+#if !defined I3D_ARCH_32_BIT && !defined I3D_ARCH_64_BIT
+    #if defined I3D_ARCH_AARCH64 || defined I3D_ARCH_AMD64
+        #define I3D_ARCH_64_BIT
+    #else
+        #define I3D_ARCH_32_BIT
+    #endif
 #endif
